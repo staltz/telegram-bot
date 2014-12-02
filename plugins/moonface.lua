0bbb19390c9fd3a301a73f63cec50e87e24d439a
magalvao <keyo@keyo.me>
2014-12-02 14:22:41 -0200

    Update moonface.lua
    Corrected unicode chars to escape chars.

--- plugins/moonface.lua
+++ plugins/moonface.lua
@@ -2,13 +2,13 @@ function run(msg, matches)
 	math.randomseed(os.time())
     x = math.random(1,3)
 	if x == 2 then
-		return "🌝"
+		return "\240\159\140\157"
 	end
 end
 
 return {
-    description = "reacts to 🌝 emoticon (33% chance)", 
-    usage = "Use 🌝 in a message ",
-    patterns = {"🌝"}, 
+    description = "reacts to :full_moon_with_face: emoticon (33% chance)", 
+    usage = "Use \240\159\140\157 in a message ",
+    patterns = {"\240\159\140\157"}, 
     run = run 
 }
