38e0965c036a78f89d6df1ad76ddee9b2bea40d0
diedona <diedona@gmail.com>
2014-12-08 22:54:06 -0200

    iniciando uns testes no politica

--- plugins/politica.lua
+++ plugins/politica.lua
@@ -1,10 +1,27 @@
+function getPoliticsWords()
+    return {
+        "dilma","pt","psdb",
+        "coxinha","prefeito",
+        "paes","cartacapital",
+        "veja","estadao","congresso",
+        "governo",".gov","politica",
+        "governador","voto","votar",
+        "votei","marina","lula","g1.com",
+        "globo.com","folha.uol"
+    };
+end
+
 function run(msg, matches)
-    return "aff politica"
+    --return "aff politica"
+    
+    print('politics');
+    
 end
 
 return {
     description = "reacts to politic chat", 
     usage = "Talk about politics ",
-    patterns = {"dilma"," pt","PT","psdb","coxinha","prefeito","paes","cartacapital","veja","estadao","congresso","governo",".gov","politica","governador","voto","votar","votei","marina","lula","g1.com","globo.com","folha.uol"}, 
+    --patterns = {"dilma","pt","psdb","coxinha","prefeito","paes","cartacapital","veja","estadao","congresso","governo",".gov","politica","governador","voto","votar","votei","marina","lula","g1.com","globo.com","folha.uol"}, 
+    patterns = {'(.*)'}, --taken from stalt's eavesdropper
     run = run 
 }
