008875ea0f6ac44934b5416c1a77b951deff9102
Andre Medeiros <andre.medeiros@futurice.com>
2014-12-10 19:44:57 +0200

    Added plugin nicole

---
+++ plugins/nicole.lua
@@ -0,0 +1,34 @@
+function get_nicole(msg, cmd)
+  print("Executando Nicole", cmd)
+  b = http.request("http://keyo.me/nicole/?q=" .. url_encode(cmd))
+  array = json:decode(b)
+  -- print("Nicole retornou", array)
+  if (array.img) then
+    file = download_to_file(array.img)
+    send_photo(get_receiver(msg), file, ok_cb, false)
+  elseif (array.msg) then
+    return array.msg
+  end
+end
+
+function url_encode(str)
+  if (str) then
+    str = string.gsub (str, "\n", "\r\n")
+    str = string.gsub (str, "([^%w %-%_%.%~])",
+        function (c) return string.format ("%%%02X", string.byte(c)) end)
+    str = string.gsub (str, " ", "+")
+  end
+  return str 
+end
+
+function run(msg, matches)
+  return get_nicole(msg, matches[1])
+end
+
+return {
+    description = "Nicole",
+    usage = "Nicole, [alguma coisa])",
+    patterns = {"^Nicole, (.*)$"},
+    run = run
+}
+
